8bc5c99b16480e84418526217a778ff10a4afe80
David Moreno García <david.mogar@gmail.com>
2015-04-28 20:33:56 +0200

    Add arguments parsing
    I moved old code to a provisional function while I add some usuful code like arguments handling and logging

--- twixer/twixer.py
+++ twixer/twixer.py
@@ -1,10 +1,28 @@
 __version__ = '0.1.0'
 
+import argparse
 import configparser
 from .facepp import API
 import tweepy
 
+
+def parse_arguments():
+    """Define this application arguments and validate input
+    :return: parsed arguments
+    """
+    parser = argparse.ArgumentParser(description='', prog='twixer')
+    parser.add_argument('account', help='target user\'s Twitter account name')
+    parser.add_argument("-v", '--verbose', help="increase output verbosity", action="store_true")
+
+    return parser.parse_args()
+
+
 def main():
+    args = parse_arguments()
+
+
+
+def foo():
     # Load Twitter keys from configuration file
     config = configparser.ConfigParser()
     config.read('twixer/config.ini')
