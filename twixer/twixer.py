fcccd1d2e42deb0640e6bba097265cd91ebb3a78
David Moreno García <david.mogar@gmail.com>
2015-04-28 21:21:04 +0200

    Refactor foo function. All is working again

--- twixer/twixer.py
+++ twixer/twixer.py
@@ -11,13 +11,27 @@ import tweepy
 logging.config.fileConfig('twixer/config/logging.conf')
 logger = logging.getLogger(__name__)
 
+# Load configuration file
+config = configparser.ConfigParser()
+config.read('twixer/config/config.ini')
+
+
+def get_user_object(username):
+    oauth = config['twitter']
+
+    auth = tweepy.OAuthHandler(oauth['consumer_key'], oauth['consumer_secret'])
+    auth.set_access_token(oauth['access_token'], oauth['access_token_secret'])
+    api = tweepy.API(auth)
+
+    return api.get_user(username)
+
 
 def parse_arguments():
     """Define this application arguments and validate input
     :return: parsed arguments
     """
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('account', help='target user\'s Twitter account name')
+    parser.add_argument('username', help='target Twitter username')
     parser.add_argument("-v", '--verbose', help="increase output verbosity", action="store_true")
 
     return parser.parse_args()
@@ -29,25 +43,14 @@ def main():
         global logger
         logger.setLevel(logging.DEBUG)
 
+    user = get_user_object(args.username)
 
-def foo():
-    # Load Twitter keys from configuration file
-    config = configparser.ConfigParser()
-    config.read('twixer/config.ini')
-    keys = config['twitter']
+    # Apply facial recognition
+    data = config['facepp']
+    api = API(data['key'], data['secret'], data['server'])
 
-    auth = tweepy.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
-    auth.set_access_token(keys['access_token'], keys['access_token_secret'])
-
-    api = tweepy.API(auth)
-
-    user = api.get_user('marianorajoy')
-
-    faceppKeys = config['facepp']
-    faceppApi = API(faceppKeys['key'], faceppKeys['secret'], faceppKeys['server'])
-
-    answer = faceppApi.detection.detect(url=user.profile_image_url)
-    if answer['face']:
-        print(user.screen_name + ": " + str(answer['face'][0]['attribute']['gender']))
+    answer = api.detection.detect(url=user.profile_image_url)
+    if len(answer['face']) == 1:
+        logger.info(user.screen_name + ": " + str(answer['face'][0]['attribute']['gender']))
     else:
-        print("Can't determine gender")
+        logger.warning("Can't determine gender")
