ea5c3e76c3a07f47dd1525551aeacefda770cd96
David Moreno García <david.mogar@gmail.com>
2015-05-13 23:27:36 +0200

    Remove unused code
    I'm moving this to a web tool so most of the old code is being replaced or moved. I'm also using a Twitter scrapper so I don't need to use tweepy anymore.

--- twixer/twixer.py
+++ twixer/twixer.py
@@ -5,7 +5,6 @@ import configparser
 import logging
 import logging.config
 import sys
-import tweepy
 
 from .lib import facepp
 
@@ -18,27 +17,6 @@ config = configparser.ConfigParser()
 if not config.read('twixer/config/config.ini'):
     logger.error('Missing config file. Have you defined it?')
     sys.exit()
-
-
-def get_user_object(username):
-    """
-    Returns an object with information about the specified user.
-
-    Parameters:
-        username: Username of the target user.
-
-    Returns:
-        An object with the user information.
-    """
-    logger.debug('Getting user information')
-
-    oauth = config['twitter']
-
-    auth = tweepy.OAuthHandler(oauth['consumer_key'], oauth['consumer_secret'])
-    auth.set_access_token(oauth['access_token'], oauth['access_token_secret'])
-    api = tweepy.API(auth)
-
-    return api.get_user(username)
 
 
 def parse_arguments():
@@ -57,20 +35,7 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
+
     if args.verbose:
         global logger
         logger.setLevel(logging.DEBUG)
-
-    user = get_user_object(args.username)
-
-    # Apply facial recognition
-    logger.debug('Applying facial recognition')
-
-    data = config['facepp']
-    api = facepp.API(data['key'], data['secret'], data['server'])
-
-    answer = api.detection.detect(url=user.profile_image_url.replace('_normal', ''))
-    if len(answer['face']) == 1:
-        logger.info(user.screen_name + ": " + str(answer['face'][0]['attribute']['gender']))
-    else:
-        logger.warning("Can't determine gender")
